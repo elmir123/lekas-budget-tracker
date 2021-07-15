7180d17a7602609154a3f13feccda49a1f362ed0
Elmir Lekovic <elmir123@gmai.com>
2021-07-15 12:37:31 -0400

    Using proper Database for the app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetDB", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
